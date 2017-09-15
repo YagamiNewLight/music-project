ea4ddd789d36a0146f3af39d2fee4d0cbf14ddc2
yeshenyue <1075049028@qq.com>
2017-09-15 11:43:25 +0800

    add ESlint

--- main.js
+++ main.js
@@ -2,28 +2,28 @@
  * Created by WKZ on 2017/9/7.
  */
 !function () {
-    var audio = document.createElement('audio');
-    var lyric = document.createElement('div');
+    var audio = document.createElement("audio");
+    var lyric = document.createElement("div");
     var $lyric = $(lyric);
     var height = -60;
-//初始化AV
+    //初始化AV
     function AVinit(){
-        var APP_ID = 'mILQIQweB355cMMwGy6EG0n0-gzGzoHsz';
-        var APP_KEY = '6BUMFbB1BVsphGS0CwCiYTDy';
+        var APP_ID = "mILQIQweB355cMMwGy6EG0n0-gzGzoHsz";
+        var APP_KEY = "6BUMFbB1BVsphGS0CwCiYTDy";
         return AV.init({
             appId: APP_ID,
             appKey: APP_KEY
         });
     }
-//getIdByUrl
+    //getIdByUrl
 
     function getParameterByName(name, url) {
         if (!url) url = window.location.href;
-        name = name.replace(/[\[\]]/g, "\\$&");
+        name = name.replace(/[\]]/g, "\\$&");
         var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
             results = regex.exec(url);
         if (!results) return null;
-        if (!results[2]) return '';
+        if (!results[2]) return "";
         return decodeURIComponent(results[2].replace(/\+/g, " "));
     }
 
@@ -31,42 +31,41 @@
         addSongInfoToPage();
         audio.oncanplay = function() {
             addListener();
-        }
+        };
         audio.onplaying = function(){
-                if($('.myData').length ===1){
-                    $($('.myData')[0]).addClass('active');
-                    $('.lyric').css('top',$('.myData')[0].clientHeight);
-                }
-                window.intervalID = setInterval(function(){
-                    var time = audio.currentTime;
-                    for(var i = 0;i<$('.myData').length;i++){
-                        var d =  $('.myData')[i].clientHeight;
-                        var dataTime = $($('.myData')[i]).attr('data-time');
-                        if(hmsToSeconds(dataTime).toFixed0() === time.toFixed0()){
-                            $($('.myData')[i]).addClass('active');
-                            if($('.myData')[i-1]){
-                                $($('.myData')[i-1]).removeClass('active');
-                            }
-                            if(d){
-                                height += d;
-                            }
-                            $('.lyric').css('top',-height);
-                        }
-                    }
-
-                },1000);
+            if($(".myData").length ===1){
+                $($(".myData")[0]).addClass("active");
+                $(".lyric").css("top",$(".myData")[0].clientHeight);
             }
+            window.intervalID = setInterval(function(){
+                var time = audio.currentTime;
+                for(var i = 0;i<$(".myData").length;i++){
+                    var d =  $(".myData")[i].clientHeight;
+                    var dataTime = $($(".myData")[i]).attr("data-time");
+                    if(hmsToSeconds(dataTime).toFixed0() === time.toFixed0()){
+                        $($(".myData")[i]).addClass("active");
+                        if($(".myData")[i-1]){
+                            $($(".myData")[i-1]).removeClass("active");
+                        }
+                        if(d){
+                            height += d;
+                        }
+                        $(".lyric").css("top",-height);
+                    }
+                }
+
+            },1000);
+        };
         audio.onpause = function(){
-            clearInterval(intervalID);
-        }
+        };
         audio.onended = function(){
             toggle();
             height = -60;
         };
     }
     function addSongInfoToPage(){
-        var query = new AV.Query('song');
-        var id = getParameterByName('id');
+        var query = new AV.Query("song");
+        var id = getParameterByName("id");
         AVinit();
         query.get(id).then(success);
     }
@@ -77,52 +76,52 @@
         dynamicAddLyrics(song);
     }
     function dynamicAddLyrics(song) {
-        lyric.classList.add('lyric');
+        lyric.classList.add("lyric");
         var array = regexParse(song);
         array.map(function(object){
             if(object){
-                var p = document.createElement('p');
-                p.setAttribute('data-time',object.time);
+                var p = document.createElement("p");
+                p.setAttribute("data-time",object.time);
                 p.textContent = object.words;
-                p.classList.add('myData');
+                p.classList.add("myData");
                 lyric.appendChild(p);
-                }
+            }
         });
-        $('.lyricWrapper').append($lyric);
+        $(".lyricWrapper").append($lyric);
     }
     function regexParse(song) {
         var regex = /^\[(.+)\](.*)$/;
-        var array  = song.attributes.lyric.split('\n');
+        var array  = song.attributes.lyric.split("\n");
         if(array.length>3){
             array = array.map(function(string){
                 var matches = string.match(regex);
                 if(matches){
-                    return {time:matches[1],words:matches[2]}
+                    return {time:matches[1],words:matches[2]};
                 }
             });
         }else{
-            return [{time:null,words:array[0]}]
+            return [{time:null,words:array[0]}];
         }
-        return array
+        return array;
     }
 
     function dynamicLoad(song){
-        $('.undergroundImg').css("background-image",'url('+song.attributes.cover+')');
-        $('.diezi>.innerPic>img').attr('src',song.attributes.cover)
+        $(".undergroundImg").css("background-image","url("+song.attributes.cover+")");
+        $(".diezi>.innerPic>img").attr("src",song.attributes.cover);
     }
     function addTitle(song) {
-        var span1 = document.createElement('span')
-        var span2 = document.createElement('span')
-        var p = document.createElement('p');
-        p.classList.add('title');
-        span1.classList.add('name');
-        span1.textContent = song.attributes.name + ' - ';
+        var span1 = document.createElement("span");
+        var span2 = document.createElement("span");
+        var p = document.createElement("p");
+        p.classList.add("title");
+        span1.classList.add("name");
+        span1.textContent = song.attributes.name + " - ";
         span2.textContent = song.attributes.singer;
         $(p).append(span1).append(span2);
-        $(p).insertBefore('.lyricWrapper');
+        $(p).insertBefore(".lyricWrapper");
     }
     function hmsToSeconds(hms){
-        var a = hms.split(':');
+        var a = hms.split(":");
         var seconds = (+a[0])* 60 + (+a[1]);
         return seconds;
     }
@@ -130,22 +129,21 @@
         return parseFloat(this.toString().replace(/(\.\d{0})\d+$/,"$1"));
     };
     function addListener(){
-        $('.play').on('click',play);
-        $('.pause').on('click',pause);
+        $(".play").on("click",play);
+        $(".pause").on("click",pause);
     }
 
     function  play() {
         audio.play();
-        $('.play').addClass('active');
-        $('.pause').removeClass('active');
-        $('.needle').addClass('active');
-        addPlayingClass($('.guanghuan'));
+        $(".play").addClass("active");
+        $(".pause").removeClass("active");
+        $(".needle").addClass("active");
+        addPlayingClass($(".guanghuan"));
     }
 
     function addPlayingClass(element){
-        if(element.hasClass('playing')){}
-        else{
-            element.addClass('playing')
+        if(!element.hasClass("playing")){
+            element.addClass("playing");
         }
     }
 
@@ -154,23 +152,23 @@
         toggle();
     }
     function toggle(){
-        var ghContainer = document.querySelector('.guanghuanImg');
-        var ghImg = ghContainer.querySelector('img');
-        var ipContainer = document.querySelector('.innerPic');
-        var ipImg = ipContainer.querySelector('img')
-        $('.needle').removeClass('active');
-        $('.pause').addClass('active');
-        $('.play').removeClass('active');
+        var ghContainer = document.querySelector(".guanghuanImg");
+        var ghImg = ghContainer.querySelector("img");
+        var ipContainer = document.querySelector(".innerPic");
+        var ipImg = ipContainer.querySelector("img");
+        $(".needle").removeClass("active");
+        $(".pause").addClass("active");
+        $(".play").removeClass("active");
         pausing(ghContainer,ghImg);
         pausing(ipContainer,ipImg);
-        $('.guanghuan').removeClass('playing');
+        $(".guanghuan").removeClass("playing");
     }
     function pausing(container,image) {
         var iTransform = getComputedStyle(image).transform;
         var cTransform = getComputedStyle(container).transform;
-        container.style.transform = cTransform === 'none'
+        container.style.transform = cTransform === "none"
             ? iTransform
-            : iTransform.concat(' ', cTransform);
+            : iTransform.concat(" ", cTransform);
     }
     main();
 }();
